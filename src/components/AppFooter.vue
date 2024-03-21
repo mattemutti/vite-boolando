8f52e8df89e6b48e9a413e354af27ca45c0ac603
mattemutti <mattemutti@gmail.com>
2024-03-21 19:30:32 +0100

    update header

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,7 +6,32 @@ export default {
 </script>
 <template>
 
-	<h2>Footer</h2>
+	<footer>
+		<div class="footer container">
+
+
+			<div class="adress">
+				<h3>Boolando s.r.l.</h3>
+				<a class="no-link" href="">Informazioni legali</a>
+				<a class="no-link" href="">Normativa sulla privacy</a>
+				<a class="no-link" href="">Diritto di recesso</a>
+
+			</div>
+			<!-- /.adress -->
+			<div class="social">
+				<h3>Trovaci anche su</h3>
+				<a class="no-link" href=""><i class="fa-brands fa-twitter"></i></a>
+				<a class="no-link" href=""><i class="fa-brands fa-facebook"></i></a>
+				<a class="no-link" href=""><i class="fa-brands fa-instagram"></i></a>
+				<a class="no-link" href=""><i class="fa-brands fa-pinterest"></i></a>
+				<a class="no-link" href=""><i class="fa-brands fa-youtube"></i></a>
+
+
+			</div>
+			<!-- /.social -->
+		</div>
+
+	</footer>
 
 </template>
 <style lang="scss"></style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,20 +6,22 @@ export default {
 </script>
 <template>
 
-	<header class="container">
-		<nav class="row">
-			<div>
-				<a class="no-link" href="">Donna</a>
-				<a class="no-link" href="">Uomo</a>
-				<a class="no-link" href="">Bambino</a>
-			</div>
+	<header>
+		<nav class="container">
+			<div class="row">
+				<div>
+					<a class="no-link" href="">Donna</a>
+					<a class="no-link" href="">Uomo</a>
+					<a class="no-link" href="">Bambino</a>
+				</div>
 
-			<img class="logo" src="../../public/images/boolean-logo.png" alt="">
+				<img class="logo" src="../../public/images/boolean-logo.png" alt="">
 
-			<div class="link">
-				<a class="no-link" href=""><i class="fa-solid fa-person"></i></a>
-				<a class="no-link" href=""><i class="fa-regular fa-heart"></i></a>
-				<a class="no-link" href=""><i class="fa-solid fa-bag-shopping"></i></a>
+				<div class="link">
+					<a class="no-link" href=""><i class="fa-solid fa-person"></i></a>
+					<a class="no-link" href=""><i class="fa-regular fa-heart"></i></a>
+					<a class="no-link" href=""><i class="fa-solid fa-bag-shopping"></i></a>
+				</div>
 			</div>
 		</nav>
 	</header>
