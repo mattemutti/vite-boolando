6918ac827d36bbb8af110703628ffd6462e5520a
mattemutti <mattemutti@gmail.com>
2024-03-21 19:36:34 +0100

    update footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,30 +7,26 @@ export default {
 <template>
 
 	<footer>
-		<div class="footer container">
-
-
-			<div class="adress">
-				<h3>Boolando s.r.l.</h3>
-				<a class="no-link" href="">Informazioni legali</a>
-				<a class="no-link" href="">Normativa sulla privacy</a>
-				<a class="no-link" href="">Diritto di recesso</a>
-
+		<div class="container">
+			<div class="row">
+				<div class="adress">
+					<h3>Boolando s.r.l.</h3>
+					<a class="no-link" href="">Informazioni legali</a>
+					<a class="no-link" href="">Normativa sulla privacy</a>
+					<a class="no-link" href="">Diritto di recesso</a>
+				</div>
+				<!-- /.adress -->
+				<div class="social">
+					<h3>Trovaci anche su</h3>
+					<a class="no-link" href=""><i class="fa-brands fa-twitter"></i></a>
+					<a class="no-link" href=""><i class="fa-brands fa-facebook"></i></a>
+					<a class="no-link" href=""><i class="fa-brands fa-instagram"></i></a>
+					<a class="no-link" href=""><i class="fa-brands fa-pinterest"></i></a>
+					<a class="no-link" href=""><i class="fa-brands fa-youtube"></i></a>
+				</div>
+				<!-- /.social -->
 			</div>
-			<!-- /.adress -->
-			<div class="social">
-				<h3>Trovaci anche su</h3>
-				<a class="no-link" href=""><i class="fa-brands fa-twitter"></i></a>
-				<a class="no-link" href=""><i class="fa-brands fa-facebook"></i></a>
-				<a class="no-link" href=""><i class="fa-brands fa-instagram"></i></a>
-				<a class="no-link" href=""><i class="fa-brands fa-pinterest"></i></a>
-				<a class="no-link" href=""><i class="fa-brands fa-youtube"></i></a>
-
-
-			</div>
-			<!-- /.social -->
 		</div>
-
 	</footer>
 
 </template>
