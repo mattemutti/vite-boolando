9105bae79cb18bbf9b7fc82893ceea3b8995da12
mattemutti <mattemutti@gmail.com>
2024-03-21 16:47:13 +0100

    add AppHeader components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script>
+
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+  }
+}
+
 </script>
-
 <!-- *********************************************************************** -->
-
 <template>
+  <AppHeader />
+
+
+
 </template>
-
 <!-- *********************************************************************** -->
-
 <style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+	name: 'AppHeader'
+}
+</script>
+<template>
+
+	<header class="header">
+		<nav class="nav container">
+			<div>
+				<a class="no-link" href="">Donna</a>
+				<a class="no-link" href="">Uomo</a>
+				<a class="no-link" href="">Bambino</a>
+			</div>
+
+			<img class="logo" src="../../public/images/boolean-logo.png" alt="">
+
+			<div class="link">
+				<a class="no-link" href=""><i class="fa-solid fa-person"></i></a>
+				<a class="no-link" href=""><i class="fa-regular fa-heart"></i></a>
+				<a class="no-link" href=""><i class="fa-solid fa-bag-shopping"></i></a>
+			</div>
+		</nav>
+	</header>
+	<!-- /.header -->
+
+
+</template>
+<style></style>
