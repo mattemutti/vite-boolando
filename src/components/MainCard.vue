5e2701af0651d6b32c0a67ae203169386b45a413
mattemutti <mattemutti@gmail.com>
2024-03-21 19:59:32 +0100

    update card

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -6,7 +6,131 @@ export default {
 </script>
 <template>
 
-	<h2>Card</h2>
+	<div class="container">
+		<div class="row">
+
+			<div class="capo1">
+				<div class="photo">
+					<div class="photo-on">
+						<img class="img-1" src="../../public/images/1.webp" alt="">
+						<!-- <img class="img-1b" src="./assets/img/1b.webp" alt=""> -->
+					</div>
+					<div class="sconto"> 30 %</div>
+					<div class="info">Sostenibilità</div>
+					<div class="heart-red">
+						<div class="heart"><i class="fa-solid fa-heart"></i></div>
+					</div>
+					<div>Levi's</div>
+					<h4>RELAXED FIT TEE UNISEX</h4>
+					<div> <span class="text-red">14,99 &euro;</span> <span class="line-through">29,99 &euro;</span>
+					</div>
+				</div>
+				<!-- 1 foto prodotto completa -->
+			</div>
+
+
+			<div class="capo1">
+				<div class="photo">
+					<div class="photo-on">
+						<img class="img-1" src="../../public/images/2.webp" alt="">
+						<!-- <img class="img-1b" src="./assets/img/2b.webp" alt=""> -->
+					</div>
+					<div class="sconto"> 30 %</div>
+					<div class="info">Sostenibilità</div>
+					<div class="heart-red">
+						<div class="heart"><i class="fa-solid fa-heart"></i></div>
+					</div>
+					<div>Levi's</div>
+					<h4>RELAXED FIT TEE UNISEX</h4>
+					<div> <span class="text-red">14,99 &euro;</span> <span class="line-through">29,99 &euro;</span>
+					</div>
+				</div>
+			</div>
+			<!-- 2 foto prodotto completa -->
+
+			<div class="capo1">
+				<div class="photo">
+					<div class="photo-on">
+						<img class="img-1" src="../../public/images/3.webp" alt="">
+						<!-- <img class="img-1b" src="./assets/img/3b.webp" alt=""> -->
+					</div>
+					<div class="sconto"> 30 %</div>
+					<div class="info">Sostenibilità</div>
+					<div class="heart-red">
+						<div class="heart"><i class="fa-solid fa-heart"></i></div>
+					</div>
+					<div>Levi's</div>
+					<h4>RELAXED FIT TEE UNISEX</h4>
+					<div> <span class="text-red">14,99 &euro;</span> <span class="line-through">29,99 &euro;</span>
+					</div>
+				</div>
+			</div>
+			<!-- 3 foto prodotto completa -->
+
+
+			<div class="capo1">
+				<div class="photo">
+					<div class="photo-on">
+						<img class="img-1" src="../../public/images/4.webp" alt="">
+						<!-- <img class="img-1b" src="./assets/img/4b.webp" alt=""> -->
+					</div>
+					<div class="sconto"> 30 %</div>
+					<div class="info">Sostenibilità</div>
+					<div class="heart-red">
+						<div class="heart"><i class="fa-solid fa-heart"></i></div>
+					</div>
+					<div>Levi's</div>
+					<h4>RELAXED FIT TEE UNISEX</h4>
+					<div> <span class="text-red">14,99 &euro;</span> <span class="line-through">29,99 &euro;</span>
+					</div>
+				</div>
+			</div>
+			<!-- 4 foto prodotto completa -->
+
+
+			<div class="capo1">
+				<div class="photo">
+					<div class="photo-on">
+						<img class="img-1" src="../../public/images/5.webp" alt="">
+						<!-- <img class="img-1b" src="./assets/img/5b.webp" alt=""> -->
+					</div>
+					<div class="sconto"> 30 %</div>
+					<div class="info">Sostenibilità</div>
+					<div class="heart-red">
+						<div class="heart"><i class="fa-solid fa-heart"></i></div>
+					</div>
+					<div>Levi's</div>
+					<h4>RELAXED FIT TEE UNISEX</h4>
+					<div> <span class="text-red">14,99 &euro;</span> <span class="line-through">29,99 &euro;</span>
+					</div>
+				</div>
+			</div>
+			<!-- 5 foto prodotto completa -->
+
+
+			<div class="capo1">
+				<div class="photo">
+					<div class="photo-on">
+						<img class="img-1" src="../../public/images/6.webp" alt="">
+						<!-- <img class="img-1b" src="./assets/img/6b.webp" alt=""> -->
+					</div>
+					<div class="sconto"> 30 %</div>
+					<div class="info">Sostenibilità</div>
+					<div class="heart-red">
+						<div class="heart"><i class="fa-solid fa-heart"></i></div>
+					</div>
+					<div>Levi's</div>
+					<h4>RELAXED FIT TEE UNISEX</h4>
+					<div> <span class="text-red">14,99 &euro;</span> <span class="line-through">29,99 &euro;</span>
+					</div>
+				</div>
+			</div>
+			<!-- 6 foto prodotto completa -->
+
+		</div>
+
+	</div>
+	<!-- /.main -->
 
 </template>
 <style lang="scss"></style>
