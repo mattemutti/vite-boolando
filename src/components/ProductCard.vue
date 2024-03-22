70cc499ff8e8b972249aa4afcb4aaec3b46997d1
mattemutti <mattemutti@gmail.com>
2024-03-22 12:50:27 +0100

    update image card and hover image

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -9,10 +9,22 @@ export default {
 
 	<div class="col">
 		<div class="card">
-			<div class="photo-on">
-				<img class="img-1" src="../../public/images/1.webp" alt="">
-				<img class="img-1b" src="../../public/images/1b.webp" alt="">
+			<div class="photo">
+
+				<div class="photo_on">
+					<img class="img_1" src="../../public/images/1.webp" alt="">
+					<img class="img_1b" src="../../public/images/1b.webp" alt="">
+				</div>
+				<!-- images -->
+				<div class="sconto"> 30 %</div>
+				<div class="info">Sostenibilità</div>
+				<div class="heart-red">
+					<div class="heart"><i class="fa-solid fa-heart"></i></div>
+				</div>
 			</div>
+			<!-- box over image -->
+
+
 			<div class="card-body">
 				<h5 class="card-title">Card title</h5>
 				<p class="card-text">Some quick example text to build on the card title and make up the bulk of
