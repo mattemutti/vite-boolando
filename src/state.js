8c4501b78c78cb05c3dbab3e3a12685ec972b185
mattemutti <mattemutti@gmail.com>
2024-03-25 18:14:22 +0100

    update axios instalation

--- src/state.js
+++ src/state.js
@@ -1,11 +1,13 @@
 import { reactive } from 'vue';
-//import axios from 'axios';
+import axios from 'axios';
 
 export const state = reactive({
 
 	message: 'This is a test',
 	products: [],
 	base_products_api_url: 'http://localhost:3000/products',
+
+
 
 	getProducts(url) {
 
