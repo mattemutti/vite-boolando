663ceebf1550019d3b3a4ee97b5bd715f0c603b7
mattemutti <mattemutti@gmail.com>
2024-03-25 16:35:27 +0100

    update

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -24,7 +24,7 @@ export default {
 	<div class="container">
 		<div class="row row-cols-1 row-cols-sm-3 row-cols-md-4 p-4 g-2">
 
-			<ProductCard :product="product" v-for="product in products" />
+			<ProductCard :product="product" v-for="product in state.products" />
 
 			<!-- <ProductCard :frontImage="product.frontImage" :backImage="product.backImage" :brand="product.brand"
 				:name="product.name" :price="product.price" :favorite="product.favorite" v-for="product in products" /> -->
--- src/state.js
+++ src/state.js
@@ -1,5 +1,5 @@
 import { reactive } from 'vue';
-// import axios from 'axios';
+//import axios from 'axios';
 
 export const state = reactive({
 
