8ed7aed5680467e4fbd394a246832b0858ed6912
mattemutti <mattemutti@gmail.com>
2024-03-26 15:32:53 +0100

    add card info product markup

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -12,6 +12,7 @@ export default {
 		return {
 			state,
 			infoDataCard: false,
+			infoProductArray: [],
 		}
 	},
 	methods: {
@@ -20,6 +21,8 @@ export default {
 			//info prod è il mio proxyarray della card cliccata 
 
 			this.infoDataCard = true;
+			this.infoProductArray = infoProd;
+			console.log(this.infoProductArray);
 		},
 		closeInfoBox() {
 			this.infoDataCard = false;
@@ -32,18 +35,56 @@ export default {
 </script>
 <template>
 
+	<div v-if="infoDataCard === true" class="info_card_screen">
+		<!-- metto le classi in italiano perchè ho importato bootstrap -->
+		<div class="contenitore">
+			<div class="riga">
+				<div class="colonna">
+					<div>Nome prodotto</div>
+					<div @click="closeInfoBox()">X</div>
+				</div>
+				<div>Descrizione prodotto</div>
+				<div>{{ }}</div>
+			</div>
+		</div>
+	</div>
 	<div class="container">
 		<div class="row row-cols-1 row-cols-sm-3 row-cols-md-4 p-4 g-2">
 
 			<ProductCard :product="product" v-for="product in state.products" @info-product="showProduct" />
 
-			<div v-if="infoDataCard === true" class="info_card_screen">
-				Dati Card
-				<div @click="closeInfoBox()">X</div>
-			</div>
+
 		</div>
 	</div>
 
 
 </template>
-<style lang="scss"></style>
+<style>
+.info_card_screen {
+
+	position: relative;
+	text-align: center;
+
+
+
+	.contenitore {
+		background-color: red;
+		position: fixed;
+		width: 600px;
+		height: 300px;
+		top: 30%;
+		left: 40%;
+		z-index: 1;
+	}
+
+	.colonna {
+		display: flex;
+		justify-content: space-between;
+		color: white;
+
+
+
+	}
+
+}
+</style>
